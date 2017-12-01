b3f69105a0d0aa0b4f1078f245846f192ebc0cf1
Jay Song <jsong022@ucr.edu>
2017-11-30 16:25:10 -0800

    Added more gameplay mechanics beyond alerts when squares are clicked

--- README.txt
+++ README.txt
@@ -2,5 +2,19 @@ A simple game based on RGB colors
 
 Made as a practice for HTML, CSS, and JavaScript
 
+- Presents a color in RGB format in the Heading.
+- Presents 6 Squares with different colors to guess from.
+- User clicks on a Square to guess that color.
+- If the color is the one represented by the RGB given:
+	the Result shows "correct" & all Squares change to that color
+- If the color guessed is not the one represented by the RGB given:
+	the Result shows "try again" & chosen Square turns "invisible" 
+
+To Add:
+- Randomize the colors (both the target color & the color choices)
+- Randomize the answer Square (currently is the same one every time)
+- Make it look pretty (get some CSS & Bootstrap practice)
+- Add different difficulties (more/less choices to choose from)
+
 Author: Jay Song
 Date: 11/30/2017
--- game.js
+++ game.js
@@ -10,6 +10,7 @@ var colors = [
 //select and save pieces of the HTML we need to manipulate
 var squares = document.getElementsByClassName("square");
 var display = document.querySelector("#display");
+var result = document.getElementById("result");
 
 targetColor = colors[3];
 
@@ -21,7 +22,20 @@ for (var i = 0; i < squares.length; i++){
 	//add initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
 	squares[i].addEventListener("click", function(){
-		var result = (this.style.backgroundColor === targetColor)? "correct!" : "wrong!";
-		alert(result);
+		if (this.style.backgroundColor === targetColor){
+			result.textContent = "Correct!";
+			changeColors(targetColor);
+		}
+		else{
+			this.style.backgroundColor = document.body.style.backgroundColor;
+			result.textContent = "Try again!"
+		}
 	});
+}
+
+//change colors of ALL squares to color string passed as argument
+function changeColors(color){
+	for (var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
 }
